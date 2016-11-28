6c212f80b823575cfbb546841ae2e4742800717e
lvt <zero.lethanh@gmail.com>
2016-11-28 22:44:47 +0900

    Initial Commit

---
+++ PhotoShare/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoShare
+//
+//  Created by THANH LEVAN on 2016/11/28.
+//  Copyright © 2016年 THANH LEVAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
